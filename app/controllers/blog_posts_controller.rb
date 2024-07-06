5615ecb9ad62d3d094cf81b110edbb880ce6509e
Matthew <waddellmp08@gmail.com>
2024-07-05 23:27:06 -0500

    Added scopes & model methods to BlogPost model

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -6,14 +6,10 @@ class BlogPostsController < ApplicationController
   # Define an instance variable to hold all blog posts
   # Instance variables can be accessed in the view
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
-  def show
-    # Handle ActiveRecord::RecordNotFound exception
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
-  end
+  def show; end
 
   def new
     @blog_post = BlogPost.new
@@ -57,11 +53,11 @@ class BlogPostsController < ApplicationController
   # Define private method to permit require :blog_post key in params object
   # Then permit and extract only :title and :body keys
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_04_041912) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_172721) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
