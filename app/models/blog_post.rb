298396a468531a368d477b56413f0cfbf73ea243
Matthew <waddellmp08@gmail.com>
2024-07-07 17:15:23 -0500

    Fix syntax error for BlogPost model arel_table ref

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,7 +3,7 @@ class BlogPost < ApplicationRecord
   validates :body, presence: true
 
   # This is a scope that returns all blog posts that have a published_at date
-  scope(:sorted, -> { order(arel_table(:published_at).desc.nulls_last) }).order(updated_at: desc)
+  scope(:sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) })
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where('published_at <= ?', Time.current) }
   scope(:scheduled, -> { where('published_at > ?', Time.current) })
