22e07d08b84d0a84fb73a8c4db40bd9f3aecb684
Matthew <waddellmp08@gmail.com>
2024-07-07 17:02:28 -0500

    Add nulls_last to blog_post scope method

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,10 +3,11 @@ class BlogPost < ApplicationRecord
   validates :body, presence: true
 
   # This is a scope that returns all blog posts that have a published_at date
+  scope(:sorted, -> { order(arel_table(:published_at).desc.nulls_last) }).order(updated_at: desc)
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where('published_at <= ?', Time.current) }
   scope(:scheduled, -> { where('published_at > ?', Time.current) })
-  scope(:sorted, -> { order(published_at: :desc, updated_at: :desc) })
+
   def draft?
     published_at.nil?
   end
